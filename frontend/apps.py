3b9362649f817d4d8832ae425d708452ab60468a
lolik444 <vojtech.barnat@gmail.com>
2023-04-06 14:40:48 +0200

    frontend init

--- frontend/apps.py
+++ frontend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PlaygroundConfig(AppConfig):
+class FrontendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playground'
+    name = 'frontend'
--- playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# URLConf
-urlpatterns = [
-    path('hi/', views.say_hi),
-    path('hi2/', views.say_hi_http_render)
-]
