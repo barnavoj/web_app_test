ea79636c679f02b2f8efc050a303535fee9a0ca9
lolik444 <vojtech.barnat@gmail.com>
2023-04-07 14:53:25 +0200

    API new view for modifiing rooms and Frontend with material UI

--- api/models.py
+++ api/models.py
@@ -14,9 +14,7 @@ def generate_unique_code():
 
 
 class Room(models.Model):
-
-    default_code = ""
-    code = models.CharField(max_length=8, default=default_code, unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('room/modify', views.CreateRoomView.as_view()),
     path('room/create', views.RoomCreate.as_view()),
     path('room/show', views.RoomView.as_view()),
 ]
